cb6dcca74c86f484a10e38aea68dc945c1fde260
msaner <msaner@fullsail.edu>
2014-06-05 18:56:58 -0700

    future year
    testing the math operator add.  adding dob to a number to get a year in
    the future.

--- madlib/main.py
+++ madlib/main.py
@@ -15,11 +15,13 @@ verb2 = raw_input("Enter another verb and hit enter.")
 pets = raw_input("Enter how many pets you have and hit enter.")
 color = raw_input("Enter your favorite color of the rainbow; (red orange yellow green blue or purple) and hit enter.")
 
+#setting up a dictionary to house values based on the different colors a user might enter
 animal = dict()
 animal = {"red":"unicorn", "orange":"beaver", "yellow":"squirrel", "green":"duck", "blue":"eagle", "purple":"t-rex"}
 
-
-
+# we're going to add the users year of birth to a number to get a year in the future by adding the two numbers
+future_date = dob + 1256
+print future_date
 
 if color in animal:
     print(animal[color])
