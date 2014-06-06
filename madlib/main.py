457b0079d86b44d1f05870d3cd318a4dd7e1822d
msaner <msaner@fullsail.edu>
2014-06-05 17:58:53 -0700

    adding user inputs
    began entering some raw_input commands to gather user responses to use
    later in the mad lib story

--- madlib/main.py
+++ madlib/main.py
@@ -4,3 +4,12 @@ Developer: Michael Saner
 Date: June 5 2014
 '''
 
+#establish prompts for users to enter information we will use later to create our story
+
+name = raw_input("Enter a persons name and hit enter.")
+dob = raw_input("Enter the year you were born and hit enter.")
+number = raw_input("Enter an even number and hit enter.")
+adjective1 = raw_input("Enter an adjective and hit enter.")
+verb1 = raw_input("Enter a verb and hit enter.")
+verb2 = raw_input("Enter another verb and hit enter.")
+pets = raw_input("")
