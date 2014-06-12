0164ac05c26066940f120a145e5d8d58d103c361
msaner <msaner@fullsail.edu>
2014-06-11 20:04:53 -0700

    made function match private attribute
    changed tot_exp to total

--- encapsulated-calculator/main.py
+++ encapsulated-calculator/main.py
@@ -35,7 +35,7 @@ class MainHandler(webapp2.RequestHandler):
         r.work_snacks = 5
         r.other = 0
         #call the getter function and print result
-        self.response.write('Robert spent $' + str(r.tot_exp) + ' on food this week.')
+        self.response.write('Robert spent $' + str(r.total) + ' on food this week.')
 
 
         #Mary
@@ -45,7 +45,7 @@ class MainHandler(webapp2.RequestHandler):
         m.dining_out = 81
         m.work_snacks = 7
         m.other = 14
-        self.response.write('Mary spent $' + str(m.tot_exp) + ' on food this week.')
+        self.response.write('Mary spent $' + str(m.total) + ' on food this week.')
 
         #James
         j = Food()
@@ -54,7 +54,7 @@ class MainHandler(webapp2.RequestHandler):
         j.dining_out = 0
         j.work_snacks = 13
         j.other = 21
-        self.response.write('James spent $' + str(j.tot_exp) + ' on food this week.')
+        self.response.write('James spent $' + str(j.total) + ' on food this week.')
 
         #Katy
         k = Food()
@@ -63,7 +63,7 @@ class MainHandler(webapp2.RequestHandler):
         k.dining_out = 21
         k.work_snacks = 4
         k.other = 0
-        self.response.write('Katy spent $' + str(k.tot_exp) + ' on food this week.')
+        self.response.write('Katy spent $' + str(k.total) + ' on food this week.')
 
         #Steve
         s = Food()
@@ -72,7 +72,7 @@ class MainHandler(webapp2.RequestHandler):
         s.dining_out = 107
         s.work_snacks = 36
         s.other = 0
-        self.response.write('Steve spent $' + str(s.tot_exp) + ' on food this week.')
+        self.response.write('Steve spent $' + str(s.total) + ' on food this week.')
 
 
 
@@ -89,7 +89,7 @@ class Food(object):
 
     #set up a getter so we can use the private data
     @property
-    def tot_exp(self): #make sure this function name matches the attribute above
+    def total(self): #make sure this function name matches the attribute above
         #calculate the weekly cost of food
         self.__total = self.groceries + self.fast_food + self.dining_out + self.work_snacks + self.other
         return self.__total
