d537b785a059e48e2d6f7a5fd8b3aae12d2b2997
msaner <msaner@fullsail.edu>
2014-06-25 20:57:58 -0700

    loading frameworks
    using objects from the urllib2 and json frameworks.

--- final/main.py
+++ final/main.py
@@ -5,7 +5,11 @@ Design Patterns for Web Programming
 Final Project
 """
 import webapp2
+from urllib2 import urlopen
+from json import load
 
+
+#Controller
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
